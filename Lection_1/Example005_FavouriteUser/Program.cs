a4a255307f88d76cd9a9711044f590bb748f37b6
Gorets <mih.kom1998@gmail.com>
2023-06-20 22:16:32 +0300

    feat: add new folder and programm Example005

---
+++ Lection_1/Example005_FavouriteUser/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Введите имя: ");
+string name = Console.ReadLine();
+
+if (name == "Gorets"){
+    Console.WriteLine("Привет, хозяин!!!");
+}else{
+    Console.Write("Привет, ");
+    Console.WriteLine(name);
+}
