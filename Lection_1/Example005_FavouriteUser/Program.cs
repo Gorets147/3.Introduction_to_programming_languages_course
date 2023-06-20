cbf477e8cf26597862caf1d2af2af2b8980e52a0
Gorets <mih.kom1998@gmail.com>
2023-06-20 22:20:16 +0300

    feat: add function ToLower in Examle005

--- Lection_1/Example005_FavouriteUser/Program.cs
+++ Lection_1/Example005_FavouriteUser/Program.cs
@@ -1,7 +1,7 @@
 ﻿System.Console.WriteLine("Введите имя: ");
 string name = Console.ReadLine();
 
-if (name == "Gorets"){
+if (name.ToLower() == "gorets"){
     Console.WriteLine("Привет, хозяин!!!");
 }else{
     Console.Write("Привет, ");
