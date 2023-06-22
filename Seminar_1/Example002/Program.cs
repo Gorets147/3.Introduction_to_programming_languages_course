027be74901fc94cc2d7533579a42169958380948
Gorets147 <mih.kom1998@gmail.com>
2023-06-22 21:47:50 +0300

    feat: add new solution for task 2

--- Seminar_1/Example002/Program.cs
+++ Seminar_1/Example002/Program.cs
@@ -1,13 +1,24 @@
 ﻿//  1. Напишите программу, которая на вход принимает два числа и проверяет,
 //    является ли первое число квадратом второго.
-System.Console.WriteLine("Введите число: ");
-int a = int.Parse(Console.ReadLine()!);
-System.Console.WriteLine("Введите число: ");
-int b = int.Parse(Console.ReadLine()!);
 
 
-if(a/b == b){
-    System.Console.WriteLine("Квадрат!");
-} else {
-    System.Console.WriteLine("Не квадрат!");
-};
+
+
+square();
+
+static void square(){
+    System.Console.WriteLine("Введите число: ");
+    int a = int.Parse(Console.ReadLine()!);
+    System.Console.WriteLine("Введите число: ");
+    int b = int.Parse(Console.ReadLine()!);
+
+
+    if(a/b == b){
+        System.Console.WriteLine("Квадрат!");
+    } else {
+        System.Console.WriteLine("Не квадрат!");
+    };
+}
+
+
+
