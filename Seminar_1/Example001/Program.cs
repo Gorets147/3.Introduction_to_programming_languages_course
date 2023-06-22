6694f872cdd587f791c283314f84ec804d9233d7
Gorets147 <mih.kom1998@gmail.com>
2023-06-22 21:01:05 +0300

    feat: add branch 1_Seminar, create programm in Example001

--- Seminar_1/Example001/Program.cs
+++ Seminar_1/Example001/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿// 0. Напишите программу, которая на вход принимает число и
+//    выдаёт его квадрат (число умноженное на само себя).
+
+
+
+System.Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine(num * num);
