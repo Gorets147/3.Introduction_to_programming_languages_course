6953d7bc9ed6de0f3d486352375fb7387f114da0
Gorets <mih.kom1998@gmail.com>
2023-06-26 00:15:12 +0300

    feat: add solution for task number 3 on HW_1_3 branch

---
+++ HW/1_3/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num == 0) {
+    Console.WriteLine("ОШИБКА");
+} else if(num % 2 == 0) {
+    Console.WriteLine("ЧЁТНОЕ");
+} else {
+    Console.WriteLine("НЕчётное");
+}
