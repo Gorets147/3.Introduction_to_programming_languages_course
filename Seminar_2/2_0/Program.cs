bd8e51e19f7211cd9256970a5ab615c60e86698a
Gorets <mih.kom1998@gmail.com>
2023-06-26 20:50:35 +0300

    feat: add task_1_0 in Seminar_2

---
+++ Seminar_2/2_0/Program.cs
@@ -0,0 +1,12 @@
+﻿// 0. Напишите программу, которая принимает на вход трёхзначное число
+//    и на выходе показывает последнюю цифру этого числа.
+
+int LastDig(int num)
+{
+    System.Console.WriteLine(num);
+    return num % 10;
+}
+
+int th_num = new Random().Next(100, 1000);
+int result = LastDig(th_num);
+System.Console.WriteLine(result);
