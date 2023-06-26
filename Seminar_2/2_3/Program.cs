728fd594ca0b9f3c2bfc2b009e53f16a071f0809
Gorets <mih.kom1998@gmail.com>
2023-06-26 21:26:04 +0300

    feat: add task_2_2 and solution for his

---
+++ Seminar_2/2_3/Program.cs
@@ -0,0 +1,11 @@
+﻿double num1 = new Random().Next(10, 100);
+double num2 = new Random().Next(1, 10);
+
+void CheckNumbers(double a, double b)
+{
+    System.Console.WriteLine($"Числа: {a}, {b}");
+    if (a % b == 0) System.Console.WriteLine("Числа кратны");
+    else System.Console.WriteLine($"Не кратны, остаток: {a % b}");
+}
+
+CheckNumbers(num1, num2);
