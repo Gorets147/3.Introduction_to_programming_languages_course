e4d043205ca72ebf668854f5ad0dafc071646d42
Gorets <mih.kom1998@gmail.com>
2023-06-26 14:15:02 +0300

    feat: add Example002 in Lection_2 write code in example002

--- Lection_2/Example001/Program.cs
+++ Lection_2/Example001/Program.cs
@@ -17,9 +17,15 @@ int a3 = 3;
 int b3 = 11;
 int c3 = 46;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));
+
+
 
 System.Console.WriteLine(max);
