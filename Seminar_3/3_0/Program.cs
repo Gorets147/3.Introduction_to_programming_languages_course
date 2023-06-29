bbc7a5ad3c2530e870894d6089af69dfb853563b
Gorets <mih.kom1998@gmail.com>
2023-06-29 20:47:25 +0300

    feat: add task 3_0 in folder Seminar_3

---
+++ Seminar_3/3_0/Program.cs
@@ -0,0 +1,17 @@
+﻿
+System.Console.WriteLine("Введите координату x: ");
+double x = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите координату y: ");
+double y = double.Parse(Console.ReadLine()!);
+
+void Quarters(double x, double y)
+{
+    if (x == 0 || y == 0) System.Console.WriteLine("Start of coordinats");
+    else if (x > 0 && y > 0) System.Console.WriteLine("1-st quarter");
+    else if (x < 0 && y > 0) System.Console.WriteLine("2-nd quarter");
+    else if (x < 0 && y < 0) System.Console.WriteLine("3-rd quarter");
+    else if (x > 0 && y < 0) System.Console.WriteLine("4-th quarter");
+}
+   
+
+Quarters(x, y);
