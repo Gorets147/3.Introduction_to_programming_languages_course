90c149a9073693fd0425d72e992a8ab038adcf07
Gorets <mih.kom1998@gmail.com>
2023-06-29 11:21:25 +0300

    feat: add solution for task in HW/2_0

---
+++ HW/2_0/Program.cs
@@ -0,0 +1,10 @@
+﻿int num = int.Parse(Console.ReadLine()!);
+SecondDig(num);
+
+void SecondDig(int num)
+{
+    System.Console.WriteLine(num);
+    num = num / 10;
+    num = num % 10;
+    System.Console.WriteLine(num);
+}
