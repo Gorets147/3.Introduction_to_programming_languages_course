eeece188c936801109041ea7cf934f75f2a37a06
Gorets <mih.kom1998@gmail.com>
2023-06-29 12:57:34 +0300

    feat: add solution task in /HW/2_2

---
+++ HW/2_2/Program.cs
@@ -0,0 +1,34 @@
+﻿System.Console.WriteLine("Enter day of the week number: ");
+int day = int.Parse(Console.ReadLine()!);
+DayOfWeek(day);
+
+void DayOfWeek(int day)
+{
+    switch (day)
+    {
+        case 1:
+            System.Console.WriteLine("Monday is working day");
+            break;
+        case 2:
+            System.Console.WriteLine("Tuesday is working day");
+            break;
+        case 3:
+            System.Console.WriteLine("Wednesday is working day");
+            break;
+        case 4:
+            System.Console.WriteLine("Thursday is working day");
+            break;
+        case 5:
+            System.Console.WriteLine("Friday is working day");
+            break;
+        case 6:
+            System.Console.WriteLine("Saturday day off");
+            break;
+        case 7:
+            System.Console.WriteLine("Sunday day off");
+            break;
+        default:
+            System.Console.WriteLine("Error!");
+            break;
+    }
+}
