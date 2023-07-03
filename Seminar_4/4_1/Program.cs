a8f8eb7cd9992eda382d9446aca15f6600824d2b
Gorets147 <mih.kom1998@gmail.com>
2023-07-03 21:30:46 +0300

    feat: add task from Seminar_4 in such folder

---
+++ Seminar_4/4_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// 1. Напишите программу, которая принимает
+//    на вход число и выдаёт количество цифр в числе.
+
+System.Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+
+int FindAmount(int num)
+{
+    int count = 0;
+    while (num != 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+System.Console.WriteLine(FindAmount(number));
+
+// Console.WriteLine("Введите число");
+// int Count (int num)
+// {
+//     int result=0;
+//     for (int i = 1; num!=0; i++)
+//     {
+//         num/=10;
+//         result=i;
+//     }
+// return result;
+// }
+// int A=int.Parse(Console.ReadLine());
+// Console.WriteLine(Count(A));
