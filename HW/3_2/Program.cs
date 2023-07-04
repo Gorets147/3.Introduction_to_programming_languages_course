9bbcf293d1520b433b8d759172d2b787a40280ce
Gorets <mih.kom1998@gmail.com>
2023-07-04 20:35:50 +0300

    feat: add solution for HW_3 from Seminar_3

---
+++ HW/3_2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+System.Console.WriteLine("Enter the number: ");
+int num = int.Parse(Console.ReadLine()!);
+Cube(num);
+
+void Cube(int n)
+{
+    
+    if (n > 0)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(Math.Pow(i, 3));
+        }
+    }
+    else
+    {
+        for (int i = -1; i >= n; i--)
+        {
+            Console.WriteLine(Math.Pow(i, 3));
+        }
+    }
+}
