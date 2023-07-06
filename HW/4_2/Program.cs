13bbfad42c30ad601e8251e04b438f11998c9dec
Gorets <mih.kom1998@gmail.com>
2023-07-06 21:46:14 +0300

    feat: add solution for task from Seminar_5

--- HW/4_2/Program.cs
+++ HW/4_2/Program.cs
@@ -5,20 +5,31 @@
 */
 
 
-void FillArray(int[] arr, int num_1, int num_2)
+void FillArray(int[] arr, int from, int to)
 {
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = new Random().Next(num_1, num_2);
+        arr[i] = new Random().Next(from, to);
     }
 }
 
 void PrintArray(int[] arr)
 {
-    for(int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
         System.Console.WriteLine(arr[i]);
     }
+}
+
+int[] EightMass(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to + 1);
+    }
+    return arr;
 }
 
 System.Console.WriteLine("Enter a range [A,B) of numbers starting from the left border (A): ");
@@ -31,3 +42,18 @@ int[] numbers = new int[8];
 
 FillArray(numbers, a, b);
 PrintArray(numbers);
+
+
+
+// void MassConf(int[] arr)
+// {
+//     for (int i = 0; i < length; i++)
+//     {
+//         arr[i] = new Random().Next();
+//     }
+// }
+// void PrintArray(int[] arr)
+// {
+//     var str = string.Join(" ", arr);
+//     Console.Write($"{str} ");
+// }
