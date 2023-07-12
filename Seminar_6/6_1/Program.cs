3a5da26b388f76bd14c91e74ca55049e56bc4100
Gorets <mih.kom1998@gmail.com>
2023-07-12 20:15:50 +0300

    feat: add solution tasks from Seminar_6

---
+++ Seminar_6/6_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// 1. Напишите программу, которая принимает на вход три числа
+//    и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+void CheckTriAngle(int side1, int side2, int side3)
+{
+    if(side1 < side2+side3 && side2 < side1+side3 && side3 < side2+side1)
+    {
+        System.Console.WriteLine("Yes");
+    }
+    else System.Console.WriteLine("No");
+}
+
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+CheckTriAngle(a, b, c);
---
+++ Seminar_6/6_4/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1,10);
+        }
+    }
+}
+
+int[,] CopyMatrix(int[,] matr)
+{
+    int[,] CopyMatr = new int[matr.GetLength(0), matr.GetLength(1)];
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            CopyMatr[i, j] = matr[i, j];
+        }
+        System.Console.WriteLine();
+    }
+    return CopyMatr;
+}
+
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+int[,] matrix2 = CopyMatrix(matrix);
+
+PrintArray(matrix);
+PrintArray(matrix2);
