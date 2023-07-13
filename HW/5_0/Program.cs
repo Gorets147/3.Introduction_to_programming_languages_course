83330e59176732754f6537a59afe7aa773557b7f
Gorets <mih.kom1998@gmail.com>
2023-07-13 19:29:34 +0300

    feat: add solution for tasks HW_5 from seminar_5

---
+++ HW/5_0/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(100, 1000);
+
+    return arr;
+}
+
+int CountEven(int[] arr)
+{
+    int res = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) res++;
+    }
+    return res;
+}
+
+System.Console.WriteLine("Введите размер массива: ");
+int num = int.Parse(Console.ReadLine()!);
+
+
+int[] mass = MassNums(num);
+
+Print(mass);
+System.Console.WriteLine(CountEven(mass));
---
+++ HW/5_1/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to + 1);
+
+    return arr;
+}
+
+int SumEven(int[] arr)
+{
+    int res = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0) res += arr[i];
+    }
+    return res;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+
+Print(mass);
+System.Console.WriteLine(SumEven(mass));
