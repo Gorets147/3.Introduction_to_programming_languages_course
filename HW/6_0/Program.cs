588aefd2c6e3a114e7dbea2d3f74194b5fdaac64
Gorets147 <mih.kom1998@gmail.com>
2023-07-14 21:42:32 +0300

    feat: add solution for tasks HW_6 from Seminar_6

---
+++ HW/6_0/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 1: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+System.Console.WriteLine("How many numbers you will enter?");
+int n = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine(CountNumbers(n));
+
+int CountNumbers(int M)
+{
+    int count = 0;
+    for (int i = 0; i < M; i++)
+    {
+        System.Console.WriteLine($"Enter {i + 1} number");
+        int num = int.Parse(Console.ReadLine()!);
+        if (num > 0) count++;
+    }
+    return count;
+}
---
+++ HW/6_1/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+System.Console.WriteLine("Введите b1: ");
+int a = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите k1: ");
+int b = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите b2: ");
+int c = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите k2: ");
+int d = int.Parse(Console.ReadLine()!);
+
+string FindPoint(double b1, double k1, double b2, double k2)
+{
+    double k_dif = k1 - k2;
+    if (k_dif != 0)
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = (k1 * x) + b1;
+        return $"({x}, {y})";
+    } 
+    else if (k1 == k2 && b1 == b2) return "Бесконечное число точек пересечения!";
+    else return "Отсутствую точки пересечения";
+}
+
+System.Console.WriteLine(FindPoint(a, b, c, d));
