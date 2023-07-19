9e78b19331e771b1e37052a811d567f1373ba51e
Gorets <mih.kom1998@gmail.com>
2023-07-19 21:36:12 +0300

    feat: add solution for HW_7

---
+++ HW/7_0/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[,] FillArray(int rows, int colums)
+{
+    double[,] matr = new double[rows, colums];
+    Random rand = new Random();
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = Math.Round(rand.NextDouble() * 10, 2);
+        }
+    }
+    return matr;
+}
+
+// double[] MassNums(int size)
+// {
+//     double[] arr = new double[size];
+//     Random rand = new Random();
+
+//     for (int i = 0; i < size; i++) arr[i] = Math.Round(rand.NextDouble() * 10, 2);
+//     return arr;
+// }
+
+
+Console.WriteLine("Введите количество строк ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов ");
+int n = int.Parse(Console.ReadLine()!);
+
+double[,] matrix = new double[m, n];
+
+double[,] matrix2 = FillArray(m, n);
+PrintArray(matrix2);
