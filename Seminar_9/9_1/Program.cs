0410895ad40b8abf79c847311eda9b89d80feed2
Gorets147 <mih.kom1998@gmail.com>
2023-07-20 21:44:03 +0300

    feat: add solution task from Seminar_9

---
+++ Seminar_9/9_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// 2. Задайте значения M и N. Напишите программу, которая 
+//    рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+void PrintNumber(int from, int to)
+{
+    if (to < from) return;
+    PrintNumber(from, to - 1);
+    System.Console.Write($"{to} ");
+}
+PrintNumber(4, 6);
---
+++ Seminar_9/9_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// 4. Напишите программу, которая на вход принимает два числа A и B,
+//    и возводит число А в целую степень B с помощью рекурсии.
+
+System.Console.WriteLine("Введите A: ");
+int A = int.Parse(System.Console.ReadLine()!);
+System.Console.WriteLine("Введите B: ");
+int B = int.Parse(System.Console.ReadLine()!);
+
+int Rec(int A, int B)
+{
+    if (B == 0) return 1;
+
+    return Rec(A, B - 1) * A;
+}
+
+Rec(A, B);
+Console.WriteLine($"{Rec(A, B)}");
