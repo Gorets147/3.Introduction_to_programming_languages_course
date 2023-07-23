aa72d53d4f2e37b5ad9c827f57e77fc4916bec32
Gorets147 <mih.kom1998@gmail.com>
2023-07-23 18:46:41 +0300

    feat: add examples from Lection_7

---
+++ Lection_7/Example001/Program.cs
@@ -0,0 +1,86 @@
+﻿// Собрать строку с числами от a до b, a ≤ b
+
+string NumbersFor(int a, int b)
+{
+ string result = String.Empty;
+ for (int i = a; i <= b; i++)
+ {
+ result += $"{i} ";
+ }
+ return result;
+}
+string NumbersRec(int a, int b)
+{
+ if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+ else return String.Empty;
+}
+Console.WriteLine(NumbersFor(1, 10)); // 1 2 3 4 5 6 7 8 9 10
+Console.WriteLine(NumbersRec(1, 10)); // 1 2 3 4 5 6 7 8 9 10
+
+// Собрать строку с числами от a до b, a ≥ b
+
+string NumbersFor(int a, int b)
+{
+ string result = String.Empty;
+ for (int i = a; i >= b; i--)
+ {
+ result += $"{i} ";
+ }
+ return result;
+}
+string NumbersRec(int a, int b)
+{
+ if (a <= b) return NumbersRec(a + 1, b) + $"{a} ";
+ else return String.Empty;
+}
+Console.WriteLine(NumbersFor(1, 10)); // 10 9 8 7 6 5 4 3 2 1
+Console.WriteLine(NumbersRec(1, 10)); // 10 9 8 7 6 5 4 3 2 1
+
+
+// Сумма чисел от 1 до n
+
+int SumFor(int n)
+{
+ int result = 0;
+ for (int i = 1; i <= n; i++) result += i;
+ return result;
+}
+int SumRec(int n)
+{
+ if (n == 0) return 0;
+ else return n + SumRec(n - 1);
+}
+Console.WriteLine(SumFor(10)); // 55
+Console.WriteLine(SumRec(10)); // 55
+
+// Факториал числа
+
+int FactorialFor(int n)
+{
+ int result = 1;
+ for (int i = 1; i <= n; i++) result *= i;
+ return result;
+}
+int FactorialRec(int n)
+{
+ if (n == 1) return 1;
+ else return n * FactorialRec(n - 1);
+}
+Console.WriteLine(FactorialFor(10)); // 3628800
+Console.WriteLine(FactorialRec(10)); // 3628800
+
+
+// Вычислить а^n
+
+int PowerFor(int a, int n)
+{ int result = 1;
+ for (int i = 1; i <= n; i++) result *= a;
+ return result;
+}
+int PowerRec(int a, int n)
+{ //return n == 0 ? 1 : PowerRec(a, n - 1) * a;
+ if (n == 0) return 1;
+ else return PowerRec(a, n - 1) * a;
+}
+Console.WriteLine(PowerFor(2, 10)); // 1024
+Console.WriteLine(PowerRec(2, 10)); // 1024
---
+++ Lection_7/Example003/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        System.Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        System.Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+
+string path = @"D:/Программирование/3.Introduction_to_programming_languages_course";
+CatalogInfo(path);
+
