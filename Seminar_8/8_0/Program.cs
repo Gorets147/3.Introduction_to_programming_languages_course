a6cbac2d99b1755ba2dea1fd095818a8b31ddc74
Gorets <mih.kom1998@gmail.com>
2023-07-24 18:30:31 +0300

    feat: add solution from Seminar_8

---
+++ Seminar_8/8_0/Program.cs
@@ -0,0 +1,50 @@
+﻿// 1. Задайте двумерный массив. Напишите программу,
+//    которая поменяет местами первую и последнюю строку массива.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to + 1);
+    return arr;
+}
+
+void ChangeRow(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        (arr[0, i], arr[arr.GetLength(0) - 1, i]) = (arr[arr.GetLength(0) - 1, i], arr[0, i]);
+    }
+}
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the min number of massive ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the max number of massive ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+
+Print(mass);
+ChangeRow(mass);
+Print(mass);
---
+++ Seminar_8/8_2/Program.cs
@@ -0,0 +1,66 @@
+﻿// 3. Составить частотный словарь элементов двумерного массива.
+//    Частотный словарь содержит информацию о том, сколько раз 
+//    встречается элемент входных данных. Значения элементов массива 0..9
+//    https://metanit.com/sharp/tutorial/4.9.php
+
+Console.Write("Кол-во строк ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Кол-во столбцов ");
+int col = int.Parse(Console.ReadLine()!);
+int[,] matrix = new int[row, col];
+
+Console.Write("Введите минимальное значение элемента массива : ");
+int min = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение элемента массива : ");
+int max = int.Parse(Console.ReadLine()!);
+
+FillArray(matrix);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+int[] array = FrequancyDictionary(matrix);
+PrintArray(array);
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix)
+{
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+	    for (int j = 0; j < matrix.GetLength(1); j++)
+	    {
+	        matrix[i, j] = new Random().Next(min, max + 1);
+	    }
+	}
+}
+
+int[] FrequancyDictionary(int[,] arr)
+{
+    int[] freq = new int[max + 1];
+
+    foreach (int i in arr)
+    {
+        freq[i]++;
+    }
+    return freq;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] != 0) System.Console.WriteLine($"{i} - {arr[i]}, ");
+    }
+}
+
+
+
