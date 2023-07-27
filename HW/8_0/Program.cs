a88b14d7d712861eb1355458ca8b895caf6c6da5
Gorets <mih.kom1998@gmail.com>
2023-07-27 16:51:00 +0300

    feat: solutions for HW_8

---
+++ HW/8_0/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+Console.Write("Кол-во строк ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Кол-во столбцов ");
+int col = int.Parse(Console.ReadLine()!);
+int[,] matrix = new int[row, col];
+
+Console.Write("Введите минимальное значение элемента массива : ");
+int min = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение элемента массива : ");
+int max = int.Parse(Console.ReadLine()!);
+
+FillArray(matrix);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+StreamlineElementOfRow(matrix);
+PrintMatrix(matrix);
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix)
+{
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+	    for (int j = 0; j < matrix.GetLength(1); j++)
+	    {
+	        matrix[i, j] = new Random().Next(min, max + 1);
+	    }
+	}
+}
+
+void StreamlineElementOfRow (int[,] matr)
+{
+    int temp = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < matr.GetLength(1); k++)
+            {
+                if (matr[i, j] < matr[i, k])
+                {
+                    temp = matr[i, j];
+                    matr[i, j] = matr[i, k];
+                    matr[i, k] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ HW/8_2/Program.cs
@@ -0,0 +1,80 @@
+﻿/*
+Задача 3: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+Console.Write("Кол-во строк 1-й матрицы: ");
+int row1 = int.Parse(Console.ReadLine()!);
+Console.Write("Кол-во столбцов 1-й матрицы: ");
+int col1 = int.Parse(Console.ReadLine()!);
+int[,] matrix1 = new int[row1, col1];
+Console.Write("Кол-во строк 2-й матрицы: ");
+int row2 = int.Parse(Console.ReadLine()!);
+Console.Write("Кол-во столбцов 2-й матрицы: ");
+int col2 = int.Parse(Console.ReadLine()!);
+int[,] matrix2 = new int[row2, col2];
+
+Console.Write("Введите минимальное значение элемента массива: ");
+int min = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение элемента массива: ");
+int max = int.Parse(Console.ReadLine()!);
+
+FillArray(matrix1, min, max);
+FillArray(matrix2, min, max);
+PrintMatrix(matrix1);
+System.Console.WriteLine("*");
+PrintMatrix(matrix2);
+System.Console.WriteLine();
+PrintMatrix(MultMatrix(matrix1, matrix2));
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix, int min, int max)
+{
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+	    for (int j = 0; j < matrix.GetLength(1); j++)
+	    {
+	        matrix[i, j] = new Random().Next(min, max + 1);
+	    }
+	}
+}
+
+int[,] MultMatrix(int[,] matr1, int[,] matr2)
+{
+    int[,] ResMatr = new int[matr1.GetLength(0), matr2.GetLength(1)];
+    if (matr1.GetLength(1) != matr2.GetLength(0)) 
+    {
+        System.Console.WriteLine("Количество столбцов 1-й матрицы должно быть равно количеству строк 2-й матрицы");
+        return ResMatr;
+    }
+    for (int i = 0; i < matr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr2.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr1.GetLength(1); k++)
+            {
+                ResMatr[i, j] += matr1[i, k] * matr2[k, j];
+            }
+        }
+
+    }
+    return ResMatr;
+}
+    
